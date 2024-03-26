fac12ac8cae2e88b7112200a75f1b9716acab037
Cao quy Le <mikamokedo@gmail.com>
2024-03-26 13:33:10 +0700

    fix: init filter

---
+++ frontend/src/pages/Project/Filter/ProjectAttribute.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ProjectAttribute = () => {
+  return <div>ProjectAttribute</div>;
+};
---
+++ frontend/src/pages/Project/Filter/ProjectType.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ProjectType = () => {
+  return <div>ProjectType</div>;
+};
