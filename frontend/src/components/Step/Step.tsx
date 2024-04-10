25fef314abc751ee854561710a57ca3ecff504b3
quyle <quy@wow-ai.com>
2024-04-10 11:15:48 +0700

    fix: chore

--- frontend/src/components/Step/Step.tsx
+++ frontend/src/components/Step/Step.tsx
@@ -1,31 +1,33 @@
-import IconCircleChecked from "@/assets/icons/IconCircleChecked"
-import IconCircleSuccess from "@/assets/icons/IconCircleSuccess"
-import IconNextStep from "@/assets/icons/IconNextStep"
+import IconCircleChecked from "@/assets/icons/IconCircleChecked";
+import IconCircleSuccess from "@/assets/icons/IconCircleSuccess";
+import IconNextStep from "@/assets/icons/IconNextStep";
 import "./Step.scss";
-import { useState } from "react";
 
 type TStep = {
-    isActive: boolean;
-    stepName: string;
-    isLastStep: boolean;
-    stepNumber:number;
-    onClick: (
-        value: number,
-    ) => void;
+  isActive: boolean;
+  stepName: string;
+  isLastStep: boolean;
+  stepNumber: number;
+  onClick: (value: number) => void;
 };
 
-export default function Step({ isActive, stepName, isLastStep, stepNumber, onClick = () => { } }: TStep) {
-
-    return (
-        <>
-            <div onClick={() => onClick(stepNumber)} className={`menu-step__tab menu-step__tab${isActive && '--active'}`}>
-                {
-                    isActive ? <IconCircleSuccess /> : <IconCircleChecked />
-                }
-                <span>{`${stepNumber}. ${stepName}`}</span>
-            </div>
-            {isLastStep && <IconNextStep />}
-        </>
-    )
-
+export default function Step({
+  isActive,
+  stepName,
+  isLastStep,
+  stepNumber,
+  onClick = () => {},
+}: TStep) {
+  return (
+    <>
+      <div
+        onClick={() => onClick(stepNumber)}
+        className={`menu-step__tab menu-step__tab${isActive && "--active"}`}
+      >
+        {isActive ? <IconCircleSuccess /> : <IconCircleChecked />}
+        <span>{`${stepNumber}. ${stepName}`}</span>
+      </div>
+      {!isLastStep && <IconNextStep />}
+    </>
+  );
 }
